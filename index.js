73e20e9fef8edfd6a83aa53fb9c665eba8379f88
kwin64 <kwin11bvb@yandex.by>
2023-01-11 15:18:12 +0300

    feat: multer (uploads images)

--- index.js
+++ index.js
@@ -2,9 +2,42 @@ import express from 'express'
 import cors from 'cors'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import multer from 'multer'
+dotenv.config()
+
+mongoose.set('strictQuery', false)
+mongoose
+	.connect(process.env.DB_URL)
+	.then(() => console.log('db connected'))
+	.catch(err => console.log('db error', err))
 
 const app = express()
+app.use(express.json())
 app.use(cors({ origin: '*' }))
+
+const storage = multer.diskStorage({
+	destination: (_, __, cb) => {
+		if (!fs.existsSync('uploads')) {
+			fs.mkdirSync('uploads')
+		}
+		cb(null, 'uploads')
+	},
+	filename: (_, file, cb) => {
+		cb(null, file.originalname)
+	}
+})
+const upload = multer({ storage })
+app.use('/uploads', express.static('uploads'))
+
+//routes
+app.post('/uploads', upload.single('image'), (req, res) => {
+	res.json({
+		url: `uploads/${req.file.originalname}`
+	})
+})
+
 const server = createServer(app)
 const io = new Server(server, { cors: { origin: '*' } })
 
