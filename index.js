9e52b3c9c39ee1c3a4a9be32c0b48ede9b8c973b
kwin64 <kwin11bvb@yandex.by>
2023-01-11 14:12:06 +0300

    feat: connect sockets

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import express from 'express'
-// import http from 'http'
-// import cors from 'cors'
-// import fs from 'fs'
-// import dotenv from 'dotenv'
-// import mongoose from 'mongoose'
-
-const PORT = 5080
+import cors from 'cors'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 
 const app = express()
+app.use(cors({ origin: '*' }))
+const server = createServer(app)
+const io = new Server(server, { cors: { origin: '*' } })
 
-app.listen(PORT, () => console.log(`server started on ${PORT}`))
+io.on('connection', socket => {
+	socket.on('join', ({ name, room }) => {
+		socket.join(room)
+
+		const response = { user: { userName: name }, message: `connected ${name}` }
+		socket.emit('message', response)
+	})
+
+	socket.on('disconnect', () => {
+		console.log('Disconnected')
+	})
+})
+
+server.listen(process.env.PORT || 8000, () => {
+	console.log('Server running on : ', process.env.PORT || 8000)
+})
